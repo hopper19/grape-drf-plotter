254360819ae3eeb6d9e0e2f99d183df81b595f74
Cuong Nguyen <37946259+hopper19@users.noreply.github.com>
2025-02-16 15:38:26 -0500

    Delete logging-exercise directory

--- logging-exercise/my_module.py
+++
@@ -1,9 +0,0 @@
-import logging 
-logger = logging.getLogger(__name__)
-
-
-def my_function():
-    logger.info("This is a log message")
-    logger.warning("This is a warning")
-    logger.error("This is an error")
-    logger.critical("This is a critical error")
